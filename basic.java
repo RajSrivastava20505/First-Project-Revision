19c966d1dc79d42147adf1b6354c5ebde5ece6da
Raj Srivastava <rajsrivastava20505@gmail.com>
2026-05-28 19:37:41 +0530

    aaded a

--- basic.java
+++ basic.java
@@ -4,7 +4,7 @@ public class basic {
     public static void main(String[] args) {
         System.out.println("hello world");
 
-        System.out.println("added a variable");
+        System.out.println("added a variable (a)");
     }
 
     int a =5;
