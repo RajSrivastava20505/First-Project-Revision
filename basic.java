f02533cb80d79054514cfee0ba7e158d9da2b2fa
Raj Srivastava <rajsrivastava20505@gmail.com>
2026-05-28 19:33:02 +0530

    added a variable

--- basic.java
+++ basic.java
@@ -3,7 +3,12 @@ package pushingDirectlyFromRemote;
 public class basic {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("added a variable");
     }
 
     int a =5;
+    int b=10;
+
+    
 }
